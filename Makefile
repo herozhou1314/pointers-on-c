bc47fdcb0e237308726943bab5e6c905d7155ef1
Nicolas Miller <nicolasmiller@gmail.com>
2012-05-01 21:56:17 -0500

    initial commit for pointers on c stuff

---
+++ Makefile
@@ -0,0 +1,7 @@
+CFLAGS=-Wall -g
+
+all:
+	gcc -o ex_ch1_1 ex_ch1_1.c
+
+clean:
+	rm -f ex_ch1_1
---
+++ ex_ch1_1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(char* argv[], int argc)
+{
+	printf("Hello, world!\n");
+	return 0;
+}
