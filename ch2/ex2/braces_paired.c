7f2448f9df5897137ca22640d749b0c1e0a8b152
Nicolas Miller <nicolasmiller@gmail.com>
2012-05-06 14:01:04 -0500

    ch2ex2 braces paired

---
+++ ch2/ex2/braces_paired.c
@@ -0,0 +1,29 @@
+/*
+Pointers on Pointers on C - Chapter 2 - Exercise 2 
+braces_paired.c - verifies that braces in a c source file are paired
+(without worrying about braces nested in comments and string or
+character literals
+
+author: nicolas steven miller <nicolasmiller@gmail.com>
+*/
+#include <stdio.h>
+
+int main(char* argv[], int argc)
+{
+	int braces = 0;
+	int ch;
+	
+	while((ch = getchar()) != EOF) {
+		if(ch == '{')
+			braces++;
+		else if(ch == '}')
+			braces--;
+	}
+	
+	if(braces == 0)
+		printf("braces balanced\n");
+	else
+		printf("braces unbalanced\n");
+	
+	return 1;
+}
