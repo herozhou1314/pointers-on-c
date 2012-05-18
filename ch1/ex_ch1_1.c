9d1dd3ecf45bc214db00d6609209fa82fcb9b028
Nicolas Steven Miller <nicolasmiller@gmail.com>
2012-05-17 23:48:05 -0500

    Update ch1/ex_ch1_1.c

--- ch1/ex_ch1_1.c
+++ ch1/ex_ch1_1.c
@@ -7,7 +7,7 @@ author:  nicolas steven miller <nicolasmiller@gmail.com>
 
 #include <stdio.h>
 
-int main(char* argv[], int argc)
+int main(int argc, char* argv[])
 {
 	printf("Hello, world!\n");
 	return 0;
